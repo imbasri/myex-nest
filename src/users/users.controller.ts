d16248c3b31b87cf4abfbe15b5296dcabe269aaf
imbasri <imbasri.dev@gmail.com>
2024-09-10 16:41:17 +0700

    Proses Menyimpan Data User Melalui Service Dan Repository.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  // take the service and inject it into the controller
+  constructor(private usersService: UsersService) {}
+  // create a new user
   @Post()
   createUser(@Body() body: CreateUserDto) {
-    return body;
+    return this.usersService.create(body.name, body.email, body.password);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
-
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+  create(name: string, email: string, password: string) {
+    // create a new user for instance
+    const user = this.userRepository.create({ name, email, password });
+    // save to the database
+    return this.userRepository.save(user);
+  }
+}
