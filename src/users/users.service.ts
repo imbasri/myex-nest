b11408e76c8a53ae9313fb2ee979060b07fbdd6c
imbasri <imbasri.dev@gmail.com>
2024-09-13 10:21:18 +0700

    Membuat Method Service Untuk Mendapatkan Data User.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,10 +7,17 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
+  findAll() {
+    return this.userRepository.find();
+  }
   create(name: string, email: string, password: string) {
     // create a new user for instance
     const user = this.userRepository.create({ name, email, password });
     // save to the database
     return this.userRepository.save(user);
+  }
+
+  findOneBy(id: number) {
+    return this.userRepository.findOneBy({ id });
   }
 }
