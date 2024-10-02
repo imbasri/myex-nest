4926deea57e0f561e353f78f3918aede8b8f1246
imbasri <imbasri.dev@gmail.com>
2024-10-02 15:18:36 +0700

    Mengubah Data Yang Ada Di Database.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -21,6 +21,12 @@ export class UsersController {
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOneBy(parseInt(id));
+  }
+
+  // update a user
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: Partial<CreateUserDto>) {
+    return this.usersService.update(parseInt(id), body, )
   }
 
   @Delete("/:id")
