1884f6103f80939994eb91fb33e9c28a9843066b
imbasri <imbasri.dev@gmail.com>
2024-10-04 13:51:26 +0700

    Setup Session Dan Cara Kerja Cookie.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['randomkey'],
+  }))
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
